7ffe8540d89261a8114c4614ee79e43da981731f
UIA412 <117601092+UIA412@users.noreply.github.com>
2023-01-24 15:05:10 +0530

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+const gulp = require("gulp");
+const gap = require("gulp-append-prepend");
+
+gulp.task("licenses", async function () {
+  gulp
+    .src("build/static/js/*chunk.js", { base: "./" })
+    .pipe(
+      gap.prependText(`/*!
+
+=========================================================
+* Black Dashboard React 
+=========================================================
+TEAM UIA412
+Developed By Kashif Raza
+
+=========================================================
+
+*/`)
+    )
+    .pipe(gulp.dest("./", { overwrite: true }));
+  gulp
+    .src("build/index.html", { base: "./" })
+    .pipe(
+      gap.prependText(`<!--
+
+=========================================================
+* Black Dashboard React 
+=========================================================
+TEAM UIA412
+Developed By Kashif Raza
+
+=========================================================
+
+-->`)
+    )
+    .pipe(gulp.dest("./", { overwrite: true }));
+  gulp
+    .src("build/static/css/*chunk.css", { base: "./" })
+    .pipe(
+      gap.prependText(`/*!
+
+=========================================================
+* Black Dashboard React 
+=========================================================
+      TEAM UIA412
+      Developed By Kashif Raza
+=========================================================
+
+*/`)
+    )
+    .pipe(gulp.dest("./", { overwrite: true }));
+  return;
+});
